78be1e10933c39b0b41598bb883a333cc3fe9791
irony <cl@iteam.se>
2014-01-27 13:15:40 +0100

    added dependencies

--- opsworks_nodejs/recipes/configure.rb
+++ opsworks_nodejs/recipes/configure.rb
@@ -9,10 +9,6 @@ node[:deploy].each do |application, deploy|
   Chef::Log.debug("node[:nodeconfig].to_json")
   Chef::Log.debug(node[:nodeconfig].to_json)
 
-  file File.join(deploy[:deploy_to], 'shared', 'node_modules', 'config', 'index.json') do
-    content JSON.dump(node[:nodeconfig].to_hash)
-  end
-
   template "#{deploy[:deploy_to]}/shared/node_modules/config/index.js" do
     cookbook 'opsworks_nodejs'
     source 'opsworks.js.erb'
